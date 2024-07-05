7bb5e71642a64a6a0ac0fc8c6527aea4edd91765
Mahan <gtmahan29@gmail.com>
2024-07-06 01:34:30 +0330

    bug fixes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -16,24 +16,6 @@ const Hero = () => {
         />
       </div>
 
-      {/* Gem images */}
-      {/* <div className="">
-        <Image
-          src="/assets/Crystals&Gems(9).png"
-          width={200}
-          height={200}
-          alt=""
-          className="absolute bottom-[10rem] -left-[4rem] z-[99] opacity-70"
-        />
-        <Image
-          src="/assets/Crystals&Gems(22).png"
-          width={200}
-          height={200}
-          alt=""
-          className="absolute top-[15rem] -right-[10rem] z-[99] opacity-70"
-        />
-      </div> */}
-
       <div className="h-[50rem] w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
@@ -51,7 +33,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mt-12 text-sm md:text-lg lg:text-2xl">
-            I'm Mahan. A Professional Web Developer
+            I&apos;m Mahan. A Professional Web Developer
           </p>
         </div>
         <a
