812f4ea384eb1bf0c241d7e0992509ca8ddcf6f4
Mahan <gtmahan29@gmail.com>
2024-07-06 01:58:51 +0330

    bug fixes

--- components/Experiences.tsx
+++ components/Experiences.tsx
@@ -10,10 +10,11 @@ const Experiences = () => {
         <span className="text-[#FFFF99]">Experiences</span>
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-y-4 gap-x-8 mt-20">
-        {workExperience.map((item: any) => (
+        {workExperience.map((item) => (
           <ExperienceCard
             title={item.title}
             id={item.id}
+            key={item.id}
             img={item.thumbnail}
             description={item.desc}
           />
