f5942bd211f2fafb46728c1725c56f0270f5e070
Mahan <gtmahan29@gmail.com>
2024-07-07 00:09:08 +0330

    minor changes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -16,9 +16,9 @@ const Hero = () => {
         />
       </div>
 
-      <div className="h-[50rem] w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
+      <div className="h-[50rem] w-full dark:bg-black-100 dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
         {/* Radial gradient for the container to give a faded look */}
-        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       </div>
 
       <div className="h-screen flex flex-col items-center justify-center relative z-10">
